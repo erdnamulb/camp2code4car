471b5dc5fda0ee24bbec64d418ad3b06c7db47f1
ABehr87 <behrandreas@hotmail.com>
2022-03-22 13:43:26 +0100

    change again

--- weltbeherrschungscode/abehr/andreas_car.py
+++ weltbeherrschungscode/abehr/andreas_car.py
@@ -3,6 +3,6 @@ import sys
 # from ../../camp2code-project_phase_1/Code/basisklassen import *
 
 project = os.path.abspath(__file__)
-origin_path = sys.path[-2]
+origin_path = sys.path[0]
 # print(project)
 print(os.path.join(os.path.dirname(origin_path)),"camp2code-project_phase_1/","Code" )
