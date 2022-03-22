d069bf0b63c19ded972e5cc98faab4230cb22c63
Andreas Behr <behrandreas@hotmail.com>
2022-03-22 13:49:54 +0100

    try again

--- weltbeherrschungscode/abehr/andreas_car.py
+++ weltbeherrschungscode/abehr/andreas_car.py
@@ -1,8 +1,7 @@
 import os
 import sys
 # from ../../camp2code-project_phase_1/Code/basisklassen import *
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(sys.path[0])), 'camp2code-project_phase_1', 'Code'))
+from basisklassen import *
+import traceback
 
-project = os.path.abspath(__file__)
-origin_path = sys.path[0]
-# print(project)
-print(os.path.join(os.path.dirname(origin_path)),"camp2code-project_phase_1/","Code" )
