be284f18c39fb983ada6712b6466e09a7a07b1b7
StefanSimon <stefan-simon@outlook.de>
2022-03-22 13:09:22 +0100

    Update Stefan

--- camp2code-project_phase_1/Code/Test_Stefan.py
+++ camp2code-project_phase_1/Code/Test_Stefan.py
@@ -1,32 +1,54 @@
 from basisklassen import *
 import traceback
 
+# ----------------- init --------------------
+bw = Back_Wheels()
+fw = Front_Wheels()
+usm = Ultrasonic()
+irm = Infrared()
+
+class BaseCar():
+    
+    def __init__(self):
+        self._steering_angle = 90
+        self._speed = 0
+        self._direction = 0
+        
+    @property
+    def speed(self):
+        return self._speed
+        
+    @property
+    def direction(self, direct: int):
+        return self._direction
+
+    def steering_angle(self, angle):
+        return self._steering_angle
+
+    def stop(self):
+        bw.stop()
+    
+    def drive(self, speed: int, direction: int):
+        self._direction = direction
+        if direction == 1: #vorwärts
+            bw.forward()
+        elif direction == -1: #rückwärts
+            bw.backward()
+        else: # alles andere = stop
+            self.stop()
+
+        self._speed = speed
+        bw.speed = speed
 
 
-try:
-    #Wackeln mit den Vorderrädern als Gruß
-    fw = Front_Wheels()
-    print(fw.get_angles)
-    fw.turn(45)
-    time.sleep(.5)
-    fw.turn(135)
-    time.sleep(.5)
-    fw.turn(90)
-    
-    
-    
-    bw = Back_Wheels()
-    bw.speed = 100
-    bw.forward()
-    time.sleep(2)
-    bw.backward()
-    time.sleep(2)
-    bw.stop()
-    
-except:
-    print('-- FEHLER --')
-    print(traceback.format_exc())
 
+
+    #def drive(dr_speed, dr_angle)
+
+
+
+@click.command()
+@click.option('--modus', '--m', type=int, default=None, help="Startet Test für Klasse direkt.")
 def main(modus):
     """Main Function for Executing the tasks
 
@@ -34,6 +56,8 @@ def main(modus):
     Args:
         modus (int): The mode that can be choosen by the user
     """
+    car = BaseCar()
+
     print('-- Fahrparcours --------------------')
     modi = {
         1: 'Fahrparcours 1 - Vorwärts und Rückwärts',
@@ -43,6 +67,7 @@ def main(modus):
         5: 'Fahrparcours 5 - Linienverfolgung',
         6: 'Fahrparcours 6 - Erweiterte Linienverfolgung',
         7: 'Fahrparcours 7 - 6. + Hinderniserkennung',
+        0: 'Ende'
     }
 
     if modus == None:
@@ -52,70 +77,78 @@ def main(modus):
             print('{i} - {name}'.format(i=m, name=modi[m]))
         print('--' * 20)
 
-    while modus == None:
-        modus = input('Wähle  (Andere Taste für Abbruch): ? ')
-        if modus in ['1', '2', '3', '4', '5']:
-            break
-        else:
-            modus = None
-            print('Getroffene Auswahl nicht möglich.')
-            quit()
-    modus = int(modus)
+    while True:
 
-    if modus == 0:
-        print('Ausrichtung der Vorderräder')
+        while modus == None:
+            modus = input('Wähle  (Andere Taste für Abbruch): ? ')
+            if modus in ['1', '2', '3', '4', '5', '6', '7', '0']:
+                break
+            else:
+                modus = None
+                print('Getroffene Auswahl nicht möglich.')
+                #quit()
+        modus = int(modus)
+
+        if modus == 1:
+            print(modi[modus])
+            car.drive(50,1)
+            time.sleep(1)
+            car.drive(50,-1)
+            time.sleep(1)
+            car.stop()
+
+        elif modus == 2:
+            print(modi[modus])
+
+        elif modus == 3:
+            print(modi[modus])
+
+        elif modus == 4:
+            print(modi[modus])
+        
+        elif modus == 5:
+            print(modi[modus])
+        
+        elif modus == 6:
+            print(modi[modus])
+        
+        elif modus == 7:
+            print(modi[modus])
+        
+        elif modus == 0:
+            print("Ende")
+            quit()
+        
+        modus = None
+        break
+
+if __name__ == '__main__':
+    
+    main()
+
+    """
+    try:
+        #Wackeln mit den Vorderrädern als Gruß
         fw = Front_Wheels()
+        print(fw.get_angles)
         fw.turn(45)
         time.sleep(.5)
         fw.turn(135)
         time.sleep(.5)
-        print('Servo der Lenkung auf 90 Grad/geradeaus ausgerichtet! (CRTL-C zum beenden)')
-        while True:
-            fw.turn(90)
-            time.sleep(1)
-
-    if modus == 1:
-        x = input('ACHTUNG! Das Auto wird ein Stück fahren!\n Dücken Sie ENTER zum Start.')
-        print('Test Hinterräder')
-        if x == '':
-            bw = Back_Wheels()
-            bw.test()
-        else:
-            print('Abruch.')
-
-    if modus == 2:
-        print('Test Vorderräder')
-        fw = Front_Wheels()
-        fw.test()
-
-    if modus == 3:
-        print('Test Ultrasonic')
-        usm = Ultrasonic()
-        usm.test()
-
-    if modus == 4:
-        print('Test Infrared')
-        irm = Infrared()
-        irm.test()
+        fw.turn(90)
+        
+        
+        
+        bw = Back_Wheels()
+        bw.speed = 100
+        bw.forward()
+        time.sleep(2)
+        bw.backward()
+        time.sleep(2)
+        bw.stop()
+        
+    except:
+        print('-- FEHLER --')
+        print(traceback.format_exc())
+    """
     
-    if modus == 5:
-        with open("config.json", "r") as f:
-            data = json.load(f)
-            turning_offset = data["turning_offset"]
-            forward_A = data["forward_A"]
-            forward_B = data["forward_B"]
-            print("Test der Lenkkalibrierung in config.json")
-            print("Turning Offset: ", turning_offset)
-            print("Forward A: ", forward_A)
-            print("Forward B: ", forward_B)
-
-        fw = Front_Wheels(turning_offset=turning_offset)
-        fw.test()
-        time.sleep(1)
-        bw = Back_Wheels(forward_A=forward_A, forward_B=forward_B)
-        bw.test()
-
-        print('Ende des Tests der Lenkkalibrierung in config.json')
-
-if __name__ == '__main__':
-    main()
