bf1a8f9d391dcd9d6363642b0328f88fab38987b
flowman23 <florian.scheid@t-online.de>
2022-03-23 11:54:19 +0100

    flobackup

--- weltbeherrschungscode/flowman23/florian_auto.py
+++ weltbeherrschungscode/flowman23/florian_auto.py
@@ -18,10 +18,7 @@ from basisklassen import *
 # print(os.path.join(os.path.dirname(os.path.dirname(path_to_myproject)), "camp2code-project_phase_1", "Code"))
 
 # ----------------- init --------------------
-bw = Back_Wheels()
-fw = Front_Wheels()
-usm = Ultrasonic()
-irm = Infrared()
+
 
 class BaseCar():
     
@@ -29,7 +26,12 @@ class BaseCar():
         self._steering_angle = 90
         self._speed = 0
         self._direction = 0
-        
+        self.bw = Back_Wheels()
+        self.fw = Front_Wheels()
+        self.usm = Ultrasonic()
+        self.irm = Infrared()
+        self.bw.stop()
+
     @property
     def speed(self):
         return self._speed
@@ -45,30 +47,41 @@ class BaseCar():
     @steering_angle.setter
     def steering_angle(self, angle):
          self._steering_angle = angle
-         fw.turn(angle)
+         self.fw.turn(angle)
 
     def stop(self):
-        bw.stop()
+        self.bw.stop()
     
     def drive(self, speed: int, direction: int):
         self._direction = direction
         if direction == 1: #vorwärts
-            bw.forward()
+            self.bw.forward()
             self._direction = 1
         elif direction == -1: #rückwärts
-            bw.backward()
+            self.bw.backward()
             self._direction = -1
         else: # alles andere = stop
             self.stop()
             self._direction = 0
 
         self._speed = speed
-        bw.speed = speed
+        self.bw.speed = speed
+
+class SonicCar(BaseCar):
+
+    def __init__(self):
+        super().__init__()
+        self._distance = 0
+
+    @property
+    def distance(self):
+        self._distance = self.usm.distance()
+        return self._distance
 
 
 def main(modus):
     
-    car = BaseCar()
+    car = SonicCar()
 
     print('------ Fahrparcours --------------------')
     modi = {
