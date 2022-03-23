f2703804562e5ea31f6535ab6f9b7adbe43b511d
ABehr87 <behrandreas@hotmail.com>
2022-03-23 13:36:39 +0100

    zwischenstand mittwoch mittag

--- weltbeherrschungscode/abehr/andreas_try.py
+++ weltbeherrschungscode/abehr/andreas_try.py
@@ -4,7 +4,7 @@ import time
 # from ../../camp2code-project_phase_1/Code/basisklassen import *
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(sys.path[0])), 'camp2code-project_phase_1', 'Code'))
 from basisklassen import *
-import traceback
+#import traceback
 
 
 
@@ -15,4 +15,4 @@ fw = Front_Wheels()
 usm = Ultrasonic()
 irm = Infrared()
 
-fw.turn(90)
+print(usm.distance())
