5e0035905b79059bc244c0c3f7f402f9fddefaf1
flowman23 <florian.scheid@t-online.de>
2022-03-23 14:40:07 +0100

    flobackup

--- weltbeherrschungscode/flowman23/florian_try_and_error.py
+++ weltbeherrschungscode/flowman23/florian_try_and_error.py
@@ -3,31 +3,9 @@ import os
 import sys
 
 import florian_auto as fa
+import loggingc2c as log
 
 car = fa.SonicCar()
-"""car.drive(20, 1)
-time.sleep(1)
-car.stop()
-car.steering_angle = 135
-time.sleep(1)
-car.steering_angle = 45
-time.sleep(1)
-car.steering_angle = 90
-car.stop()"""
 
-"""messungen = 50
-for i in range(messungen):
-    print(car.distance)
-    time.sleep(1)"""
-
-
-distance = car.distance
-car.drive(20,1)
-while distance > 7 or distance < 0:
-    distance = car.distance
-    print("Abstand zum Hindernis", distance)
-    time.sleep(.1)
-car.stop()
-print("Auto angehalten")
-
+log.makedatabase("flodb")
 
