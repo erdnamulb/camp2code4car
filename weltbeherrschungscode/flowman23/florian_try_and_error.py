41eb9b0144cd0384bbbd24aa01c22fcd0a2cd31d
flowman23 <florian.scheid@t-online.de>
2022-03-23 13:17:15 +0100

    flobackup

--- weltbeherrschungscode/flowman23/florian_try_and_error.py
+++ weltbeherrschungscode/flowman23/florian_try_and_error.py
@@ -4,15 +4,6 @@ import sys
 
 import florian_auto as fa
 
-# aktuellen Pfad herausfinden:
-path_to_myproject = sys.path[0]
-# mit dirname zweimal nach oben springen und dann mit join in die unterordner wechseln
-# anschliessend mit sys.path.append den zu durchsuchenden Systempfad erweitern auf diesen ordner
-# dieser wird dann auch nach dem basisklassen.py durchsucht
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(path_to_myproject)), "camp2code-project_phase_1", "Code"))
-
-
-
 car = fa.SonicCar()
 """car.drive(20, 1)
 time.sleep(1)
@@ -24,6 +15,19 @@ time.sleep(1)
 car.steering_angle = 90
 car.stop()"""
 
-print(car.distance)
+"""messungen = 50
+for i in range(messungen):
+    print(car.distance)
+    time.sleep(1)"""
+
+
+distance = car.distance
+car.drive(20,1)
+while distance > 7 or distance < 0:
+    distance = car.distance
+    print("Abstand zum Hindernis", distance)
+    time.sleep(.1)
+car.stop()
+print("Auto angehalten")
 
 
