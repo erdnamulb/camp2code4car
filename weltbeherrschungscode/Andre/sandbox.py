bffda6adca206eca3f986b221bb42b6adcb9f731
Andre Blum <erdna.mulb@gmail.com>
2022-03-23 14:33:38 +0100

    loggingc2c neu, logging.py gelöscht

--- weltbeherrschungscode/Andre/sandbox.py
+++ weltbeherrschungscode/Andre/sandbox.py
@@ -1,35 +1,7 @@
-def parc1(): 
-    print("Fahrparcours 1 - Vorwärts und Rückwärts")
-
-def parc2(): 
-    print("Fahrparcours 2 - Kreisfahrt mit maximalem Lenkwinkel")
-
-def parc3(): 
-    print("Fahrparcours 3 - Vorwärtsfahrt bis Hindernis")
-
-def parc4(): 
-    print("Fahrparcours 4 - Erkundungstour mit Hindernis")
-
-def quit(): 
-    print("Beende das Programm")
-    
-def handle_menu(menu):
-    while True:
-        for index, item in enumerate(menu, 1):
-            print("{}  {}".format(index, item[0]))
-        choice = int(input("Ihre Wahl? ")) - 1
-        if 0 <= choice < len(menu):
-            menu[choice][1]()
-        else:
-            print("Bitte nur Zahlen im Bereich 1 - {} eingeben".format(
-                                                                    len(menu)))
-
-menu = [
-    ["- langsam Vorwärts und Rückwärts", parc1],
-    ["- Kreisfahrt mit maximalem Lenkwinkel", parc2],
-    ["- Vorwärtsfahrt bis Hindernis", parc3],
-    ["- Erkundungstour", parc4],
-    ["- Beenden", quit]
-]
-
-handle_menu(menu)
+import loggingc2c as lg
+lg.makedatabase('cardata.db')
+print('fertig')
+lg.add_usm('cardata.db',42)
+print('geschrieben')
+lg.read_usm('cardata.db')
+print('lesen fertig')
