3ea81ecf0b6252f94cd9a6529299b2b3d0dece83
ABehr87 <behrandreas@hotmail.com>
2022-03-28 16:07:58 +0200

    Richtige Auswertung InfraSensoren

--- weltbeherrschungscode/abehr/andreas_car_0_2.py
+++ weltbeherrschungscode/abehr/andreas_car_0_2.py
@@ -132,37 +132,39 @@ def main(modus, car: SensorCar):
             max_off_track_count =25
             turning_max = 45
             
-            distance = car.distance
+            
             car.drive(30,1)
+            lt_status_now = car.irm.read_digital()
+            distance = car.distance
             while distance > 12 or distance < 0:
                 distance = car.distance
                 lt_status_now = car.irm.read_digital()
                 print(lt_status_now)
                 # Angle calculate
-                if	lt_status_now == [1,1,0,1,1]:
+                if	lt_status_now == [0,0,1,0,0]:
                     step = 0	
-                elif lt_status_now == [1,0,0,1,1] or lt_status_now == [1,1,0,0,1]:
+                elif lt_status_now == [0,1,1,0,0] or lt_status_now == [0,0,1,1,0]:
                     step = a_step
-                elif lt_status_now == [1,0,1,1,1] or lt_status_now == [1,1,1,0,1]:
+                elif lt_status_now == [0,1,0,0,0] or lt_status_now == [0,0,0,1,0]:
                     step = b_step
-                elif lt_status_now == [0,0,1,1,1] or lt_status_now == [1,1,1,0,0]:
+                elif lt_status_now == [1,1,0,0,0] or lt_status_now == [0,0,0,1,1]:
                     step = c_step
-                elif lt_status_now == [0,1,1,1,1] or lt_status_now == [1,1,1,1,0]:
+                elif lt_status_now == [1,0,0,0,0] or lt_status_now == [0,0,0,0,1]:
                     step = d_step
 
                 # Direction calculate
-                if	lt_status_now == [1,1,0,1,1]:
+                if	lt_status_now == [0,0,1,0,0]:
                     off_track_count = 0
-                    car.steering_angle =90
+                    car.steering_angle= 90
                 # turn right
-                elif lt_status_now in ([1,0,0,1,1],[1,0,1,1,1],[0,0,1,1,1],[0,1,1,1,1]):
+                elif lt_status_now in ([0,1,1,0,0],[0,1,0,0,0],[1,1,0,0,0],[1,0,0,0,0]):
                     off_track_count = 0
                     turning_angle = int(90 - step)
                 # turn left
-                elif lt_status_now in ([1,1,0,0,1],[1,1,1,0,1],[1,1,1,0,0],[1,1,1,1,0]):
+                elif lt_status_now in ([0,0,1,1,0],[0,0,0,1,0],[0,0,0,1,1],[0,0,0,0,1]):
                     off_track_count = 0
                     turning_angle = int(90 + step)
-                elif lt_status_now == [1,1,1,1,1]:
+                elif lt_status_now == [0,0,0,0,0]:
                     off_track_count += 1
                     if off_track_count > max_off_track_count:
                         #tmp_angle = -(turning_angle - 90) + 90
@@ -173,7 +175,7 @@ def main(modus, car: SensorCar):
 
                         while True:
                             lt_status = car.irm.read_digital()
-                            if lt_status[2] == 0:
+                            if lt_status[2] == 1:
                                 break
                         car.stop()
 
@@ -193,6 +195,7 @@ def main(modus, car: SensorCar):
         
         elif modus == 6:
             print(modi[modus])
+            
         
         elif modus == 7:
             print(modi[modus])
