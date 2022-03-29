668f43d2d9037acb2af6a8a49cfe872433450efc
StefanSimon <stefan-simon@outlook.de>
2022-03-29 08:42:08 +0200

    Umstellung von IR Analog auf Digital

--- weltbeherrschungscode/auto_code.py
+++ weltbeherrschungscode/auto_code.py
@@ -98,11 +98,23 @@ class SensorCar(SonicCar):
 
     def __init__(self):
         super().__init__()
-        self.irm = Infrared()
         # Setup DataFrame and DataBase
         self.df = log.init_dataframe()
         self._db_path = f"{sys.path[0]}/logdata.sqlite"
         log.makedatabase_singletable(self._db_path)
+
+        # IR anlegen
+        try:
+            path= os.path.join(os.path.dirname(os.path.dirname(sys.path[0])), 'camp2code-project_phase_1', 'Code')
+            with open(path +"/config.json", "r") as f:
+                data = json.load(f)
+                ir_references = data["ir_references"]
+                print("IR References: ", ir_references)
+        except:
+            print("config.json nicht gefunden")
+        
+        self.irm = Infrared(ir_references)
+            
        
     def drive(self, speed: int, direction: int):
         """Overloaded function from BaseCar. Added loggin
@@ -131,17 +143,49 @@ class SensorCar(SonicCar):
 
     
     @property
-    def read_ir_sensors(self) -> list:
+    def read_ir_analog(self) -> list:
         """Reads the value of the infrared module as analog.
         Returns:
             [list]: List of bytes of the measurement of each sensor read in as analog values. 
         """
         return self.irm.read_analog()
+    
+    @property
+    def read_ir_digital(self) -> list:
+        """Reads the value of the infrared module as digital.
+        Returns:
+            [list]: List of digitized measurement of the sensors using the reference as threshold. 
+        """
+        return self.irm.read_digital()
+    
+    def calibrate_ir(self):
+        self.irm.cali_references()
+
+        path= os.path.join(os.path.dirname(os.path.dirname(sys.path[0])), 'camp2code-project_phase_1', 'Code')
+        with open(path +"/config.json") as fin:
+            data = json.load(fin)
+            
+        with open(path +"/config.json", "w") as fout:
+            data['ir_references'] = list(self.irm._references)
+            fout.write(json.dumps(data))
+
+        print(data)
+
 
     def log(self):
         """Function to create log entries in the dataframe
         """
-        log.add_row_df(self.df, self.distance, self.read_ir_sensors , self.speed, self.direction, self.steering_angle)
+        log.add_row_df(self.df, self.distance, self.read_ir_digital , self.speed, self.direction, self.steering_angle)
+
+    @property
+    def log_and_read_values(self):
+        """Function to read IR values, US values and write log to dataframe
+        """
+        distance = self.distance
+        ir_sensors = self.read_ir_digital
+        log.add_row_df(self.df, distance, ir_sensors , self.speed, self.direction, self.steering_angle)
+        return distance, ir_sensors
+
 
     def write_log_to_db(self):
         """Function to save log Dataframe to sqlite DB
