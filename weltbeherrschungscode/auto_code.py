bcf8516fef160f68308c8754279a594dea9f5438
StefanSimon <45081165+StefanSimon@users.noreply.github.com>
2022-05-23 07:16:51 +0200

    Update auto_code.py

--- weltbeherrschungscode/auto_code.py
+++ weltbeherrschungscode/auto_code.py
@@ -226,6 +226,103 @@ class CamCar(SensorCar):
         ret, frame = self.VideoCapture.read()
         frame = cv2.flip(frame, -1)
         return frame, ret if return_ret_value else frame
+    
+    def region_of_interest(self, edges):
+        height, width = edges.shape
+        mask = np.zeros_like(edges)
+
+        # only focus bottom half of the screen
+        polygon = np.array([[
+            (0, 4/5 * height),
+            (1/4 * width, 1/3 * height),
+            (3/4 * width, 1/3 * height),
+            (width, 4/5 * height),
+        ]], np.int32)
+
+        cv2.fillPoly(mask, polygon, 255)
+        cropped_edges = cv2.bitwise_and(edges, mask)
+        return cropped_edges
+
+    def detect_line_segments(self, cropped_edges):
+        # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+        rho = 1  # distance precision in pixel, i.e. 1 pixel
+        angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+        min_threshold = 10  # minimal of votes
+        line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, np.array([]), minLineLength=8, maxLineGap=4)
+        return line_segments
+
+    def draw_line_segments(self, line_segments, img):
+        img2 = img.copy()
+        for line in line_segments:
+            x1,y1,x2,y2 = line[0]
+            cv2.line(img2,(x1,y1),(x2,y2),(0,0,255),4)
+        return img2
+    
+    def average_slope_intercept(self, frame, line_segments):
+        """
+        This function combines line segments into one or two lane lines
+        If all line slopes are < 0: then we only have detected left lane
+        If all line slopes are > 0: then we only have detected right lane
+        """
+        lane_lines = []
+        if line_segments is None:
+            print('No line_segment segments detected')
+            return lane_lines
+
+        height, width, _ = frame.shape
+        left_fit = []
+        right_fit = []
+
+        boundary = 1/3
+        left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 1/3 of the screen
+        right_region_boundary = width * boundary # right lane line segment should be on right 1/3 of the screen
+
+        for line_segment in line_segments:
+            for x1, y1, x2, y2 in line_segment:
+                if x1 == x2:
+                    print('skipping vertical line segment (slope=inf): %s' % line_segment)
+                    continue
+                fit = np.polyfit((x1, x2), (y1, y2), 1)
+                slope = fit[0]
+                intercept = fit[1]
+                if slope < 0:
+                    if x1 < left_region_boundary and x2 < left_region_boundary:
+                        left_fit.append((slope, intercept))
+                else:
+                    if x1 > right_region_boundary and x2 > right_region_boundary:
+                        right_fit.append((slope, intercept))
+
+        left_fit_average = np.average(left_fit, axis=0)
+        if len(left_fit) > 0:
+            lane_lines.append(self.make_points(frame, left_fit_average))
+
+        right_fit_average = np.average(right_fit, axis=0)
+        if len(right_fit) > 0:
+            lane_lines.append(self.make_points(frame, right_fit_average))
+
+        print('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+        return lane_lines
+    
+    def make_points(self, frame, line):
+        height, width, _ = frame.shape
+        slope, intercept = line
+        y1 = height  # bottom of the frame
+        y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+
+        # bound the coordinates within the frame
+        x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+        x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+        return [[x1, y1, x2, y2]]
+    
+    def display_lines(self, frame, lines, line_color=(0, 255, 0), line_width=2):
+        line_image = np.zeros_like(frame)
+        if lines is not None:
+            for line in lines:
+                for x1, y1, x2, y2 in line:
+                    cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+        line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+        return line_image
 
     def testCam(self):
         """TEXT
@@ -239,14 +336,27 @@ class CamCar(SensorCar):
                 print("Can't receive frame (stream end?). Exiting ...")
                 break
             # Bildmanipulation ----------
-            #frame_to_display = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-            #lower_blue = np.array([60,40,40])
-            #upper_blue = np.array([150,255,255])
-            #frame_to_display = cv2.inRange(frame_to_display, lower_blue,upper_blue)
-            frame_to_display = frame
+            #frame_blur=cv2.blur(frame,(5,5))
+            
+            frame_in_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            lower_blue = np.array([60,40,40]) #([60,100,75])
+            upper_blue = np.array([120,255,255])
+            frame_hsv_in_color_range = cv2.inRange(frame_in_hsv, lower_blue,upper_blue)
+            #frame_hsv_in_color_range=cv2.blur(frame_hsv_in_color_range,(5,5))
+            frame_canny_edges = cv2.Canny(frame_hsv_in_color_range,200, 400)
+            frame_cuted_regions = self.region_of_interest(frame_canny_edges)
+            line_segments = self.detect_line_segments(frame_cuted_regions)
+            
+            # Display Frame with marks
+            """frame_with_marks = self.draw_line_segments(line_segments, frame)
+            cv2.imshow("Display window (press q to quit)", frame_with_marks)"""
+            
+            lane_lines = self.average_slope_intercept(frame, line_segments)
+            lane_lines_image = self.display_lines(frame, lane_lines)
+            
             # ---------------------------
             # Display des Frames
-            cv2.imshow("Display window (press q to quit)", frame_to_display)
+            cv2.imshow("Display window (press q to quit)", lane_lines_image)
             # Ende bei Drücken der Taste q
             if cv2.waitKey(1) == ord('q'):
                 break
@@ -258,3 +368,9 @@ class CamCar(SensorCar):
         """Releases the camera so it can be used by other programs.
         """
         self.VideoCapture.release()
+
+if __name__ == '__main__':
+    # car anlegen
+    car = CamCar()
+    car.testCam()
+    car.release()
