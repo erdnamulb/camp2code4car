88414c039c1be64df97e6be3708e934b8fc630f8
apthimm <apthimm@web.de>
2022-05-31 14:00:20 +0200

    update

--- weltbeherrschungscode/Allan/Projektphase2/dash_ui_camcar.py
+++ weltbeherrschungscode/Allan/Projektphase2/dash_ui_camcar.py
@@ -1,5 +1,5 @@
 import sys
-from cv2 import VideoCapture, cuda_GpuMat_Allocator
+import socket
 import dash
 import pandas as pd
 from dash import dcc
@@ -12,15 +12,13 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 from sqlite3 import connect
 import datetime as dt
-from flask import Flask, Response
+from flask import Flask, Response, request
+import auto_code as ac
 
-from requests import Response
-from frame_editing import *
-from auto_code_Allan import CamCar
+car = ac.CamCar()
 
-app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY])
 server = Flask(__name__)
-
+app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY])
 #app = dash.Dash(__name__)
 df = pd.DataFrame()
 conn = connect(f'{sys.path[0]}/logdata.sqlite')
@@ -33,6 +31,30 @@ df['time'] = df.apply(
 #print(df)
 features = df.columns[1:len(df.columns)-1]
 
+def get_ip_address():
+    """Ermittlung der IP-Adresse im Netzwerk
+    Returns:
+        str: lokale IP-Adresse
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    socket_ip = s.getsockname()[0]
+    s.close()
+    return socket_ip
+
+
+@server.route('/video_feed')
+def video_feed():
+    """Will return the video feed from the camera
+    Returns:
+        Response: Response object with the video feed
+    """
+    return Response(car.get_image_bytes(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
 speed_max = df['speed'].max()
 speed_min = df['speed'].min()
 speed_mean = round(df['speed'].mean(),2)
@@ -44,12 +66,6 @@ drivetime_str = str(drivetime_tot) + " s"
 distance_tot = round(drivetime_tot * speed_mean * (30/40), 1) # ca. 30cm bei Geschwindigkeit 40 pro s
 distance_str = str(distance_tot) + " cm"
 
-@server.route('/video_feed')
-def video_feed():
-    cam = CamCar()
-    
-    return Response(cam.get_image_frame(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 card_speed_max = dbc.Card(
     [
@@ -153,19 +169,24 @@ COL_Manual = [
         ),
     ]
 
-COL_Joystick = [
-        dbc.Col([
-        html.H5("Car-Stick"),
-        daq.Joystick(id="joystick", size=100, className="mb-3")],
+COL_Joystick = dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.H5("Car-Stick"),
+                daq.Joystick(id="joystick", size=100, className="mb-3")
+            ],
         width=4,
-    ),
-    dbc.Col(
+        ),
+        dbc.Col(
         [
             html.P(id="value_joystick"),
         ],
         width=4,
-    ),
+        ),
     ]
+)
+
 
 COL_Slider = [
     dbc.Col([html.H5("vMax:")], width=5),
@@ -229,7 +250,7 @@ COL_Dropdown = [  # Auswahlfeld
 COL_LiveView = [ 
     dbc.Row(
             [  
-                html.H2(
+                html.H5(
                     id="titel_Kamera",
                     children="LiveView",
                     style={
@@ -331,8 +352,7 @@ dbc.Container\
 
 @app.callback(
     Output(component_id='dataplot', component_property='figure'),
-    [Input(component_id='choose_data', component_property='value')],)
-
+    [Input(component_id='choose_data', component_property='value')])
 def graph_update(value_of_input_component):
     fig = px.line(df, x=pd.to_datetime(df['time']), y=df[value_of_input_component],
     title="Gruppe 3 Fahrdaten", 
@@ -348,7 +368,6 @@ def graph_update(value_of_input_component):
 )
 
 def joystick_values(angle, force, switch, max_Speed):
-    car = CamCar()
     """Steuerung über Joystick
         berechnet anhand der Joystick-Werte den Lenkeinschlag
         und mit der eingestellten Maximalgeschwindigkeit
@@ -394,6 +413,5 @@ def joystick_values(angle, force, switch, max_Speed):
     return debug
 
 
-
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False, host=get_ip_address())
